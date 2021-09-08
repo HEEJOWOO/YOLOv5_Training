e43e38a34d242bc06bce07014ca24dd0ce371fd3
HEEJO WOO <61686244+HEEJOWOO@users.noreply.github.com>
2021-09-08 22:07:31 +0900

    Update dataset_glob.py

--- dataset_glob.py
+++ dataset_glob.py
@@ -1,11 +1,11 @@
 from glob import glob
  
-# dataset_root : 경로 설정 필요
-train_img_list = glob('dataset_root/train/images/*.jpg')
-valid_img_list = glob('dataset_root/valid/images/*.jpg')
+#/datasetroot : 경로 설정 필요
+train_img_list = glob('/datasetroot/train/images/*.jpg')
+valid_img_list = glob('/datasetroot/valid/images/*.jpg')
  
-with open('dataset_root/train.txt', 'w') as f:
+with open('/dataset_root/train.txt', 'w') as f:
 	f.write('\n'.join(train_img_list) + '\n')
     
-with open('dataset_root/valid.txt', 'w') as f:
+with open('/dataset_root/valid.txt', 'w') as f:
 	f.write('\n'.join(valid_img_list) + '\n')
